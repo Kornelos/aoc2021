cb959bd8a0063cf00e6d40602365f0a8ae047962
Kornel Skorka <kornel.skorka@allegro.pl>
2021-12-01 15:18:10 +0100

    day 1 part 2

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,14 +1,35 @@
 import java.io.File
 
 fun main() {
-    var lastDepth = Int.MAX_VALUE
+    val file = File("/Users/kornel.skorka/IdeaProjects/aoc-2021/src/main/resources/day1-data")
+    partOne(file)
+    partTwo(file)
+}
+
+private fun partTwo(file: File) {
+    var lastDepthSum = Int.MAX_VALUE
     var count = 0
-        File("/Users/kornel.skorka/IdeaProjects/aoc-2021/src/main/resources/day1-data").useLines {
-        it.forEach { depth ->
-            if (depth.toInt() > lastDepth) {
+    file.useLines {
+        it.map(String::toInt).windowed(3).forEach { window ->
+            val depthSum = window.sum()
+            if (depthSum > lastDepthSum) {
                 count += 1
             }
-            lastDepth = depth.toInt()
+            lastDepthSum = depthSum
+        }
+    }
+    println(count)
+}
+
+private fun partOne(file: File) {
+    var lastDepth = Int.MAX_VALUE
+    var count = 0
+    file.useLines {
+        it.map(String::toInt).forEach { depth ->
+            if (depth > lastDepth) {
+                count += 1
+            }
+            lastDepth = depth
         }
     }
     println(count)
